e52b6052b739b86a3c791c1534c96b01c865d514
kms160526 <kms160526@gmail.com>
2021-03-26 13:10:36 +0900

    6.5.3 객체타입

---
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,116 @@
+package org.zerock.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j
+public class SampleController {
+    // 6.3.4 @initBinder
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+    }
+
+    @RequestMapping("")
+    public void basic() {
+
+        log.info("basic................");
+    }
+
+    @RequestMapping(value = "/basic", method = {RequestMethod.GET, RequestMethod.POST})
+    public void basicGet() {
+        log.info("basic get............");
+    }
+
+    @GetMapping("/basicOnlyGet")
+    public void basicGet2() {
+        log.info("basic get only get............");
+    }
+
+    @GetMapping("/ex01")
+    public String ex01(SampleDTO dto) {
+        log.info("" + dto);
+
+        return "ex01";
+    }
+
+    // 파라미터의 수집과 변환
+    @GetMapping("/ex02")
+    public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+
+        log.info("name : " + name);
+        log.info("age : " + age);
+        return "ex02";
+    }
+
+    // 리스트 처리
+    @GetMapping("/ex02List")
+    public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+        log.info("ids: " + ids);
+
+        return "ex02List";
+    }
+
+    // 배열 처리
+    @GetMapping("/ex02Array")
+    public String ex02Arrat(@RequestParam("ids") String[] ids){
+        log.info("array ids: " + Arrays.toString(ids));
+
+        return "ex02Array";
+    }
+
+    // 객체리스트
+    @GetMapping("/ex02Bean")
+    public String ex02Bean(SampleDTOList list){
+        log.info("list dtos: " + list);
+
+        return "ex02Bean";
+    }
+
+    @GetMapping("/ex03")
+    public String ex03(TodoDTO todo){
+        log.info("todo : " + todo);
+        return "ex03";
+    }
+
+    // ModelAttribute 어노테이션
+    @GetMapping("/ex04")
+    public String ex04(SampleDTO dto, @ModelAttribute("page") int page){
+        log.info("dto : " + dto);
+        log.info("page : " + page);
+
+        return "/sample/ex04";
+    }
+
+    // 6.5.1 void 타입
+    @GetMapping("/ex05")
+    public void ex05(){
+        log.info("ex05..........");
+    }
+
+    // 6.5.3 객체 타입
+    @GetMapping("/ex06")
+    public @ResponseBody SampleDTO ex06(){
+        log.info("/ex06.........");
+        SampleDTO dto = new SampleDTO();
+        dto.setAge(10);
+        dto.setName("홍길동");
+
+        return dto;
+    }
+
+
+}
---
+++ ex01/src/main/webapp/WEB-INF/views/sample/ex04.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kimminsu
+  Date: 2021/03/26
+  Time: 11:23 오전
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Typee" content="text/html charset=UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h2>SAMPLEDTO ${sampleDTO}</h2>
+    <h2>PAGE ${page}</h2>
+</body>
+</html>
