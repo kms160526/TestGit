47e93d2fc0aba7f7776380833d7fa81ec23ce6c6
kms160526 <kms160526@gmail.com>
2021-03-26 13:25:23 +0900

    6.5.4 ResponseEntity 타입

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -2,6 +2,9 @@ package org.zerock.controller;
 
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -112,5 +115,17 @@ public class SampleController {
         return dto;
     }
 
+    // ResponseEntity 타입
+    @GetMapping("/ex07")
+    public ResponseEntity<String> ex07(){
+        log.info("/ex07..........");
+
+        //  {"name" : "홍길동" }
+        String msg = "{\"name\": \"홍길동\"}";
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content-Type", "application/json;charset=UTF-8");
+
+        return new ResponseEntity<>(msg, header, HttpStatus.OK);
+    }
 
 }
