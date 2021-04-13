481aff5f3bb78dc1b989336b2769926c7c6f5b87
kms160526 <kms160526@gmail.com>
2021-04-13 12:51:23 +0900

    16.2.2 객체의 반환

--- ex03/src/main/java/org/zerock/controller/SampleController.java
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.SampleVO;
 
 @RestController
 @RequestMapping("/sample")
@@ -19,5 +20,21 @@ public class SampleController {
         return "안녕하세요";
 
     }
+
+    // SampleVO를 리턴하는 메서드
+    @GetMapping(value = "/getSample", produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
+    public SampleVO getSample(){
+
+        return new SampleVO(112, "스타", "로드");
+    }
+
+    @GetMapping(value = "/getSample2")
+    public SampleVO getSample2(){
+        return new SampleVO(113, "로켓", "라쿤");
+
+    }
+
 
 }
