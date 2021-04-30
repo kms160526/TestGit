9dc063af9900b5bf0295e966a0641dda6ff27271
kms160526 <kms160526@gmail.com>
2021-04-30 15:34:32 +0900

    37 어노테이션을 이용하는 스프링 시큐리티 설정 ex06p

--- SpringEx/ex06p/src/main/java/org/zerock/controller/SampleController.java
+++ SpringEx/ex06p/src/main/java/org/zerock/controller/SampleController.java
@@ -1,6 +1,8 @@
 package org.zerock.controller;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +30,19 @@ public class SampleController {
         log.info("admin only");
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MEMBER')")
+    @GetMapping("/annoMember")
+    public void doMember2(){
+
+        log.info("logined annotation member");
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/annoAdmin")
+    public void doAdmin2(){
+
+        log.info("admin annotation only");
+    }
 
 
 }
